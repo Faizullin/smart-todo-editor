46fbd541f0e995d9d051db2573782d39c8a72739
Faizullin <osmanfajzulla@gmail.com>
2023-09-27 08:06:24 +0600

    save

--- angularapp/src/app/_features/auth/login/login.component.ts
+++ angularapp/src/app/_features/auth/login/login.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  form: FormGroup;
 
+  constructor(
+    //private authService: AuthService,
+    private router: Router
+  ) {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+
+  submit() {
+    if (!this.form.invalid) {
+      //this.authService.login(this.form.value).subscribe(() => {
+      //  this.authService.getUserData().subscribe(() => {
+      //    this.router.navigate(['/']);
+      //  })
+      //});
+    }
+  }
 }
